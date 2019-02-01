abdda4608b7feaff40e8092202cf82c234c79797
tawachan <tawachan@pixiv.com>
2019-02-01 15:35:39 +0900

    use typescript

--- App.tsx
+++ App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default class App extends React.Component {
-  render() {
+  public render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
